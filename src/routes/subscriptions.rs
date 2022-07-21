6867157aa4973de65e9081b7be7331f3ba2c354c
alonlong <alonlong@163.com>
2022-07-21 19:45:07 +0800

    add logs for subscriptions api

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,6 +12,7 @@ pub struct FormData {
 
 // Extract form data using serde.
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!("Saving new subscriber details in the database");
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, created_at) VALUES ($1, $2, $3, $4)
@@ -24,9 +25,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await;
     match result {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
